b8cfb4bf15f1a1d05f8475093588fec8ca1c359c
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-01-19 10:11:22 +0900

    feat(COMPONENTS): LOCATION LIST - 기본 레이아웃 & 데이터포맷

--- src/components/locationList.tsx
+++ src/components/locationList.tsx
@@ -5,12 +5,27 @@ interface LocationListProps {
 }
 
 const LocationList: React.FC<LocationListProps> = () => {
-    const [locations, setLocations] = useState<Array<any>>([]);
+    const [locations, setLocations] = useState<Array<{
+        name: string,
+        location: String,
+        locationDetail: string,
+        notice: string
+    }>>([]);
 
     useEffect(() => {
         setLocations([
-            'test1',
-            'test2'
+            {
+                name: '집',
+                location: '서울역',
+                locationDetail: '2번출구',
+                notice: '노숙자 김씨를 찾아주세요.'
+            },
+            {
+                name: '회사',
+                location: '서울역',
+                locationDetail: '3번출구',
+                notice: '왕초 이씨를 찾아주세요.'
+            }
         ]);
     }, []);
 
@@ -22,10 +37,16 @@ const LocationList: React.FC<LocationListProps> = () => {
                     <h2>저장된 위치정보가 없습니다.</h2>
                 }
                 {
-                    locations.map((location, idx) => {
+                    locations.map((item, idx) => {
                         return (
                             <li key={idx}>
-                                {location}
+                                <p>
+                                    {item.name}<br/>
+                                    {item.location}<br/>
+                                    {item.locationDetail}<br/>
+                                    {item.notice}<br/>
+                                </p>
+                                <button type="button">삭제</button>
                             </li>
                         );
                     })
