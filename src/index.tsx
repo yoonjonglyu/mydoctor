fa50b2ef5cd1d29d466590292c50bf2ebcf58203
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-01-19 09:24:13 +0900

    feat: REACT ENTRY - BIND APP ENTRY

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './app';
+
 const render = (App: React.FC) => {
     ReactDOM.render(
         <App />,
         document.querySelector('#app')
     );
 };
+
+render(App);
