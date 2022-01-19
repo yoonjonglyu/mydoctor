2589d263c378ac43793dfc5a6a610421ee3bd622
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-01-19 09:29:34 +0900

    feat: APP ENTRY - 루트 라우팅 메인 컴포넌트

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import Main from './pages/main';
+
 const App: React.FC = () => {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={"hi"} />
+                <Route path="/" element={<Main />} />
             </Routes>
         </Router>
     );
