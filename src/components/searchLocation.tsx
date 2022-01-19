0612b0bd6dce69f2005bbe1a4b60e261fb900e92
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-01-19 18:30:33 +0900

    feat(COMPONENTS): SEARCH LOCATION - API 연결

--- src/components/searchLocation.tsx
+++ src/components/searchLocation.tsx
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import Local from '../lib/api/kakaoLocal';
+
+import env from '../../env';
 
 interface SearchLocationProps {
 
 }
 
 const SearchLocation: React.FC<SearchLocationProps> = () => {
+    const [keyword, setKeyword] = useState('');
+
+    const SearchLocation = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const location = new Local(env.kakaoLocal);
+        if (keyword.length > 0) {
+            const search = await location.getAddress(keyword);
+            if (search) {
+                console.log(search);
+            } else {
+                alert('API 요청 오류');
+            }
+            setKeyword('');
+        }
+    }
+    const handleKeyword = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setKeyword(e.target.value);
+    }
+
     return (
-        <form data-testid="location-form">
-            <input type="text" placeholder="주소 검색" />
-            <button type="button">지도로 검색</button>
-        </form>
+        <>
+            <form data-testid="location-form" onSubmit={SearchLocation}>
+                <script type="text/javascript" src="https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=vjx3cs0ck2"></script>
+                <input
+                    type="text"
+                    value={keyword}
+                    onChange={handleKeyword}
+                    placeholder="주소 검색"
+                />
+                <button type="button">지도로 검색</button>
+            </form>
+            <div></div>
+        </>
     );
 }
 
