e50897925d9c099fdb7b9d19e387050025765fce
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-01-19 17:03:49 +0900

    feat(PAGE): MAIN - BIND SEARCH LOCATION

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import LocationList from '../components/locationList';
+import SearchLocation from '../components/searchLocation';
 
 interface MainProps {
 
@@ -10,6 +11,7 @@ const Main: React.FC<MainProps> = () => {
     return (
         <main role="main">
             <h1>Hi MyDoctor</h1>
+            <SearchLocation />
             <LocationList />
         </main>
     );
