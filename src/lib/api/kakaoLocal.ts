d67d5fb48ee9d923513c1c521d7042db515f2929
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-01-19 16:42:46 +0900

    feat(API): KAKAO LOCAL - INIT

---
+++ src/lib/api/kakaoLocal.ts
@@ -0,0 +1,22 @@
+import Axios from 'axios';
+
+class KakaoLocal {
+    url: string
+    key: string
+    constructor(key: string) {
+        this.url = "https://dapi.kakao.com/v2/local/search/address.json";
+        this.key = key;
+    }
+
+    async getAddress(query: string) {
+        const { data } = await Axios.get(`${this.url}?query=${query}`, {
+            headers: {
+                Authorization: `KakaoAK ${this.key}`
+            },
+        });
+
+        return data;
+    }
+}
+
+export default KakaoLocal;
