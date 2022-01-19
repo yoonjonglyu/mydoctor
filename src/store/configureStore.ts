c8ceb704c8563e060aa52af32a17e716db961baa
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-01-19 18:56:47 +0900

    feat(STORE): CONFIG STORE - BIND LOCATION LIST

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,9 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import LocationListReducer from './locationList';
+
 const Store = configureStore({
     reducer: {
-        
+        locationList: LocationListReducer,
     }
-}); 
+});
+
+export type RootState = ReturnType<typeof Store.getState>
+export type AppDispatch = typeof Store.dispatch
 
 export default Store;
