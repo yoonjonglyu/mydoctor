9d8a95d59a0dbd76cf5a8537f78dba8c927c37c1
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-01-19 19:17:29 +0900

    feat(COMPONENTS): LOCATION LIST - 로컬스토리지 & 리덕스

--- src/components/locationList.tsx
+++ src/components/locationList.tsx
@@ -1,32 +1,45 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from '../store/configureStore';
+
+import { setLocationList } from '../store/locationList';
 
 interface LocationListProps {
 
 }
 
 const LocationList: React.FC<LocationListProps> = () => {
-    const [locations, setLocations] = useState<Array<{
-        name: string,
-        location: String,
-        locationDetail: string,
-        notice: string
-    }>>([]);
+    const locations = useSelector((state: RootState) => state.locationList.locationList);
+    const dispatch = useDispatch();
 
-    useEffect(() => {
-        setLocations([
+    useEffect(() => { // init dummy data
+        const cache = localStorage.getItem('locationList');
+        if (!cache) {
+            localStorage.setItem(
+                'locationList',
+                JSON.stringify([
+                    {
+                        name: '집',
+                        location: '서울역',
+                        locationDetail: '2번출구',
+                        notice: '노숙자 김씨를 찾아주세요.'
+                    },
+                    {
+                        name: '회사',
+                        location: '서울역',
+                        locationDetail: '3번출구',
+                        notice: '왕초 이씨를 찾아주세요.'
+                    }
+                ])
+            );
+        }
+
+        dispatch(setLocationList(
             {
-                name: '집',
-                location: '서울역',
-                locationDetail: '2번출구',
-                notice: '노숙자 김씨를 찾아주세요.'
-            },
-            {
-                name: '회사',
-                location: '서울역',
-                locationDetail: '3번출구',
-                notice: '왕초 이씨를 찾아주세요.'
+                locationList: cache ? JSON.parse(cache) : []
             }
-        ]);
+        ));
+
     }, []);
 
     return (
@@ -41,10 +54,10 @@ const LocationList: React.FC<LocationListProps> = () => {
                         return (
                             <li key={idx}>
                                 <p>
-                                    {item.name}<br/>
-                                    {item.location}<br/>
-                                    {item.locationDetail}<br/>
-                                    {item.notice}<br/>
+                                    {item.name}<br />
+                                    {item.location}<br />
+                                    {item.locationDetail}<br />
+                                    {item.notice}<br />
                                 </p>
                                 <button type="button">삭제</button>
                             </li>
