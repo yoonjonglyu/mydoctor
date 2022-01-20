df32e363cd1e31450cb6db8225b37b6f63161bc6
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-01-20 12:49:08 +0900

    feat(COMPONENTS): SELECT LOCATION - INIT

---
+++ src/components/selectLocation.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import { useSearchLocations } from '../lib/custom/locations';
+
+interface SelectLocationProps {
+
+}
+
+const SelectLocation: React.FC<SelectLocationProps> = () => {
+    const {
+        searchLocations,
+        setSearchLocations,
+    } = useSearchLocations();
+
+    return (
+        <div
+            data-tesdid="select-box"
+            style={{
+                height: "400px",
+                overflow: "auto",
+                border: "1px solid"
+            }}
+        >
+            <ul>
+                {
+                    searchLocations.length === 0 &&
+                    <h2>검색 결과가 없습니다.</h2>
+                }
+                {
+                    searchLocations.map((item, idx) => {
+                        return (
+                            <li>
+                                <p>
+                                    test
+                                </p>
+                            </li>
+                        );
+                    })
+                }
+            </ul>
+        </div>
+    );
+}
+
+export default SelectLocation;
