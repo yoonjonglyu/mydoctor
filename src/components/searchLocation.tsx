44c363bf44fd5a6ab24162b9cc8c3b7e8a94f043
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-01-20 15:28:13 +0900

    feat(COMPONENTS): SEARCH LOCATION - 검색결과 & 데이터 포맷

--- src/components/searchLocation.tsx
+++ src/components/searchLocation.tsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState, useRef } from 'react';
 
+import SelectLocation from './selectLocation';
+
 import Local from '../lib/api/kakaoLocal';
 import NaverMap from '../lib/custom/naverMap';
+import { useSearchLocations } from '../lib/custom/locations';
 
 import env from '../../env';
 
@@ -11,14 +14,29 @@ interface SearchLocationProps {
 
 const SearchLocation: React.FC<SearchLocationProps> = () => {
     const [keyword, setKeyword] = useState('');
+    const [currentPage, setCurrentPage] = useState(1);
+    const {
+        setSearchLocations,
+    } = useSearchLocations()
 
     const SearchLocation = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         const location = new Local(env.kakaoLocal);
         if (keyword.length > 0) {
-            const search = await location.getAddress(keyword);
+            const search = await location.getAddress(keyword, currentPage);
             if (search) {
-                console.log(search);
+                const state = search.documents.map((el: any) => ({
+                    addressName: el.address ? el.address.address_name : el.road_address.address_name,
+                    x: el.address ? el.address.x : el.road_address.x,
+                    y: el.address ? el.address.y : el.road_address.y,
+                }));
+                if (!search.meta.isEnd) {
+                    setCurrentPage(currentPage + 1);
+                    setSearchLocations(state);
+                } else {
+                    setCurrentPage(1);
+                    setSearchLocations(state);
+                }
             } else {
                 alert('API 요청 오류');
             }
@@ -41,6 +59,15 @@ const SearchLocation: React.FC<SearchLocationProps> = () => {
 
     return (
         <>
+            <div
+                id="map"
+                style={{
+                    width: "50%",
+                    height: "300px"
+                }}
+                ref={mapBox}
+            >
+            </div>
             <form data-testid="location-form" onSubmit={SearchLocation}>
                 <input
                     type="text"
@@ -48,18 +75,9 @@ const SearchLocation: React.FC<SearchLocationProps> = () => {
                     onChange={handleKeyword}
                     placeholder="주소 검색"
                 />
-                <button type="button">지도로 검색</button>
+                <button type="submit">검색</button>
             </form>
-            <div
-                id="map"
-                style={{
-                    width: "100%",
-                    height: "300px"
-                }}
-                onClick={naverMap}
-                ref={mapBox}
-            >
-            </div>
+            <SelectLocation />
         </>
     );
 }
