659d47d5af42d02d8a9abbabfbb035e69606aa1f
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-01-20 13:01:10 +0900

    feat(API): KAKAO LOCAL - 키워드 검색

--- src/lib/api/kakaoLocal.ts
+++ src/lib/api/kakaoLocal.ts
@@ -4,19 +4,32 @@ class KakaoLocal {
     url: string
     key: string
     constructor(key: string) {
-        this.url = "https://dapi.kakao.com/v2/local/search/address.json";
+        this.url = "https://dapi.kakao.com/v2/local/search/";
         this.key = key;
     }
 
     async getAddress(query: string, page: number = 1, size: number = 10) {
         try {
-            const { data } = await Axios.get(`${this.url}?analyze_type=similar&page=${page}&size=${size}&query=${encodeURIComponent(query)}`, {
+            const { data } = await Axios.get(`${this.url}address.json?analyze_type=similar&page=${page}&size=${size}&query=${encodeURIComponent(query)}`, {
                 headers: {
                     Authorization: `KakaoAK ${this.key}`
                 },
             });
             return data;
-        } catch(e){
+        } catch (e) {
+            console.error(e);
+            return false;
+        }
+    }
+    async getKeyword(query: string, page: number = 1, size: number = 10) {
+        try {
+            const { data } = await Axios.get(`${this.url}keyword.json?sort=accuracy&page=${page}&size=${size}&query=${encodeURIComponent(query)}`, {
+                headers: {
+                    Authorization: `KakaoAK ${this.key}`
+                },
+            });
+            return data;
+        } catch (e) {
             console.error(e);
             return false;
         }
