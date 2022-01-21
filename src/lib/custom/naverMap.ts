804313444706d8e41d291c565c592e55c9a0dfb0
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-01-21 10:58:27 +0900

    feat(CUSTOM): NAVER MAP - 마커 & INFO & GEO

--- src/lib/custom/naverMap.ts
+++ src/lib/custom/naverMap.ts
@@ -22,18 +22,91 @@ function NaverMap(props: NaverMapProps) {
     if (!document.getElementById("naverMap")) {
         const naverMapSciprt = document.createElement('script');
         naverMapSciprt.id = "naverMap";
-        naverMapSciprt.src = url;
+        naverMapSciprt.src = `${url}&submodules=geocoder`;
         document.body.appendChild(naverMapSciprt);
     }
 
-    const initMap = () => {
-        const map = new naver.maps.Map(ref.current, {
-            center: new naver.maps.LatLng(y, x),
-            zoom: 14
-        });
-    }
 
+    const initMap = () => {
+        const map = setMap();
+        const marker = setMarker();
+        const infoWindow = new naver.maps.InfoWindow({
+            anchorSkew: true
+        });
+        initGeocoder();
+
+        function setMap() {
+            const result = new naver.maps.Map(ref.current, {
+                center: new naver.maps.LatLng(y, x),
+                zoom: 16,
+                mapTypeControl: true
+            });
+            result.setCursor('pointer');
+
+            return result;
+        }
+        function setMarker() {
+            const result = new naver.maps.Marker({
+                position: new naver.maps.LatLng(y, x),
+                map: map
+            });
+            naver.maps.Event.addListener(map, 'click', function (e: any) {
+                marker.setPosition(e.latlng);
+            });
+            return result;
+        }
+
+        function searchCoordinateToAddress(latlng: { x: number, y: number }) {
+            infoWindow.close();
+
+            naver.maps.Service.reverseGeocode({
+                coords: latlng,
+                orders: [
+                    naver.maps.Service.OrderType.ADDR,
+                    naver.maps.Service.OrderType.ROAD_ADDR
+                ].join(',')
+            }, (status: number, response: any) => {
+                if (status === naver.maps.Service.Status.ERROR) {
+                    if (!latlng) {
+                        return alert('ReverseGeocode Error, Please check latlng');
+                    }
+                    if (latlng.toString) {
+                        return alert('ReverseGeocode Error, latlng:' + latlng.toString());
+                    }
+                    if (latlng.x && latlng.y) {
+                        return alert('ReverseGeocode Error, x:' + latlng.x + ', y:' + latlng.y);
+                    }
+                    return alert('ReverseGeocode Error, Please check latlng');
+                }
+
+                const address = response.v2.address,
+                    htmlAddresses = [];
+
+                if (address.jibunAddress !== '') {
+                    htmlAddresses.push('[지번 주소] ' + address.jibunAddress);
+                }
+
+                if (address.roadAddress !== '') {
+                    htmlAddresses.push('[도로명 주소] ' + address.roadAddress);
+                }
+
+                infoWindow.setContent([
+                    '<div style="padding:10px;min-width:200px;line-height:150%;">',
+                    '<h4 style="margin-top:5px;">검색 좌표</h4><br />',
+                    htmlAddresses.join('<br />'),
+                    '</div>'
+                ].join('\n'));
+
+                infoWindow.open(map, latlng);
+            });
+        }
+        function initGeocoder() {
+            map.addListener('click', function (e: any) {
+                searchCoordinateToAddress(e.coord);
+            });
+            searchCoordinateToAddress({ x, y });
+        }
+    }
     return initMap;
 }
-
 export default NaverMap;
