752fa2b1f083412722c0ee3cf84b0a6d9ef25753
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-01-21 11:33:36 +0900

    feat(COMPONENTS): LOCATION MAP - 주소 확정 기능

--- src/components/locationMap.tsx
+++ src/components/locationMap.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 
 import NaverMap from '../lib/custom/naverMap';
 
@@ -11,8 +11,12 @@ interface LocationMapProps {
 }
 
 const LocationMap: React.FC<LocationMapProps> = ({ x, y, handleSelect }) => {
+    const [currentLocation, setCurrentLocation] = useState<{ addressName: string, x: number, y: number }>({
+        addressName: '',
+        x: 0,
+        y: 0
+    });
     const mapBox = useRef(null);
-
     const naverMap = NaverMap({
         url: `https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${env.naverMap}`,
         ref: mapBox,
@@ -20,23 +24,64 @@ const LocationMap: React.FC<LocationMapProps> = ({ x, y, handleSelect }) => {
         y: y,
     });
 
+    const naverSelect = (address: { jibunAddress: string, roadAddress: string }, xy: { x: number, y: number }) => {
+        const addressName = address.roadAddress ? address.roadAddress : address.jibunAddress;
+        setCurrentLocation({
+            addressName,
+            x: xy.x,
+            y: xy.y
+        });
+    }
+    const confirmLocation = () => {
+        handleSelect(currentLocation.addressName, currentLocation.x, currentLocation.y);
+    }
     useEffect(() => {
-        const initMap = setTimeout(naverMap, 100);
+        const initMap = setTimeout(() => {
+            naverMap(naverSelect);
+        }, 100);
         return () => {
             clearTimeout(initMap);
         }
     }, []);
 
     return (
-        <div
-            id="map"
+        <article
+            data-testid="location-box"
             style={{
-                width: "100%",
-                height: "400px"
+                position: "relative"
             }}
-            ref={mapBox}
         >
-        </div>
+            <div
+                id="map"
+                style={{
+                    width: "100%",
+                    height: "400px"
+                }}
+                ref={mapBox}
+            />
+            <div
+                style={{
+                    position: "absolute",
+                    bottom: 0,
+                    width: "100%",
+                    height: "100px",
+                    textAlign: "center",
+                    color: "#fff",
+                    background: "rgb(1, 1, 1, 50%)",
+                }}
+            >
+                <p>
+                    {currentLocation.addressName}<br />
+                    해당 주소가 맞습니까?
+                </p>
+                <button
+                    type="button"
+                    onClick={confirmLocation}
+                >
+                    확인
+                </button>
+            </div>
+        </article>
     );
 }
 
